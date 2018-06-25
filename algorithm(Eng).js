4b6ebd0b321af869f7a47d65805a3f7fea2c6a52
Jae Hyun An <38463426+goongoguma@users.noreply.github.com>
2018-06-25 17:08:39 +0900

    JavaScript-algorithm-quiz
    JavaScript-algorithm-quiz in English

---
+++ algorithm(Eng).js
@@ -0,0 +1,34 @@
+/******************************
+ * CODING CHALLENGE 1
+ */
+
+ /*
+ Mark and John are trying to compare their BMI(Body Mass Index), which is calculated using the formula : 
+ BMI = mass / height^2 = mass / (height * height).
+ (mass in kg and height in meter).
+
+ 1. Store Mark's and John's mass and height in variables.  
+ 2. Calculate both thir BMIs
+ 3. Create a boolean variable containing information about wherther Mark has a height BMI than John.
+ 4. Print a string to the console containing the variable from step 3. ( Something like "Is Mark's BMI higher than John's? true").
+ */
+
+//  1. 
+var MarkMass = 70;
+var MarkHeight = 1.80;
+var JohnMass = 65;
+var JohnHeight = 1.70;
+
+// 2.
+var MarkBMI = MarkMass / (MarkHeight*MarkHeight);
+console.log(MarkBMI)
+var JohnBMI = JohnMass / (JohnHeight*JohnHeight);
+console.log(JohnBMI)
+
+//3. 
+var compare = MarkBMI > JohnBMI;
+console.log(compare);
+
+//4. Is Mark's BMI higher than John's? true
+
+console.log('Is Mark\'s BMI higher than John\'s?+ '+ compare);
