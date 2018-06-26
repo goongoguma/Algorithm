0c8710f81c62673149c6208f67cbacfe46dd2b71
Jae Hyun An <38463426+goongoguma@users.noreply.github.com>
2018-06-26 16:01:55 +0900

    JavaScript-algorithm-quiz

--- algorithm(Eng).js
+++ algorithm(Eng).js
@@ -98,3 +98,53 @@ if (TeamJohn > TeamMike && TeamJohn > TeamMary){
   console.log('Draw');
 }
 */
+
+
+/******************************
+* CODING CHALLENGE 3
+*/
+
+// John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+
+// To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill 
+// when the bill is less than $50, 15% when the bill is between $50 and $200 and 10% if the bill is more than $200.
+ 
+//  function bill(payment){
+//   if(payment < 50) {
+//     return payment*.2;
+//   } else if(50 <= payment <= 200) {
+//     return payment*.15;
+//   } else {
+//     return  payment*.1;
+//   }
+// }
+  
+/*
+function tipCalculator(bill) {
+  var percentage;
+  if (bill < 50) {
+    percentage = .2;
+  } else if (bill >= 50 && bill < 200) {
+    percentage = .15;
+  } else {
+    percentage = .1;
+  }
+  return percentage * bill;
+}
+*/
+// In the end, John would like to have 2 arrays:
+
+// 1) Containing all three tips (one for each bill) 
+    /*var payments = [124, 48, 268];
+    var tips = [bill(payments[0]), bill(payments[1]), bill(payments[2])]
+    console.log(tips);*/
+
+    // 2) Containing all three final paid amounts (bill + tip)
+    /*var finpayment = [payments[0] + tips[0], payments[1] + tips[1], payments[2] + tips[2] ]
+    console.log(tips, finpayment);*/
+
+    // (NOTE: To calculate 20% of a value, simply multiply if with 20/100 = 0.2)
+
+
+
+
