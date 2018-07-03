b60917c007187a9e17bb98b30e53ae72461dc2ad
Jae Hyun An <38463426+goongoguma@users.noreply.github.com>
2018-07-03 22:31:00 +0900

    JavaScript-algorithm-quiz

--- algorithm(Eng).js
+++ algorithm(Eng).js
@@ -219,3 +219,108 @@ if(avgCalc(john.tips) > avgCalc(mark.tips)){
   console.log("mark paid more tips ");
 }
 */
+
+
+/*********************************************************************************************
+* CODING CHALLENGE 6
+*/
+
+
+// Let's build a fun quiz-game in the console!!
+
+// 1. Build a function constructor called Question to describe a question. A question should include:
+// a) question itself
+// b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+// c) correct answer (I would use a number for this)
+// 2. Create a couple of questions using the constructor
+// 3. Store them all inside an array
+// 4. Select one random question and log it on the console, together with the possible answers
+// (each question should have a number) (Hint: write a method for the Question objects for this task).
+// 5. Use the 'prompt' function to ask the user for the correct answer. 
+// The user should input the number of correct answer such as you displayed it on Task 4.
+// 6. Check if the answer is correct and print to the console whether the answer is correct or not
+// (Hint: write another method for this).
+// 7. Suppose this code would be a plugin for other programmers to use in their code. 
+// So make sure that all your code is private and doesn't interfere with the other programmers code
+// (Hint: we learned a special technique to do exactly that).
+/*
+ (function() {
+  function Question(question,answers,correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+  }
+  
+  Question.prototype.displayQuestion = function() {
+    console.log(this.question);
+    for (var i = 0; i < this.answers.length; i++) {
+      console.log(i + ':' + this.answers[i]);
+    }
+  }
+  
+  Question.prototype.checkAnswer = function(ans) {
+    if (ans === this.correct) {
+      console.log('correct answer!'); 
+    } else {
+      console.log('Wrong answer. Try again!')
+    }
+  }
+  
+  var q1 = new Question('Is JavaScript the collest programming language in the world?',['yes','no'],0);
+  
+  var q2 = new Question('What is the name of this course\'s teacher?',['John','Micheal','Jonas'],2);
+  
+  var q3 = new Question('What does best describe coding?'['Boring','Hard','Fun','Tediuos'],2);
+  
+  var questions = [q1, q2, q3];
+  
+  var n = Math.floor(Math.random() * questions.length);
+  
+  questions[n].displayQuestion();
+  
+  var answer = parseInt(prompt('Please select the correct answer.'));
+  
+  questions[n].checkAnswer(answer);
+  
+ })();
+*/
+// var Question = function(question,answer1,answer2){
+//     this.Quiz = console.log(question),
+//     this.Answer1 = console.log(answer1),
+//     this.Answer2 = console.log(answer2),
+//     this.prompt = prompt('What is the answer?')
+//     this.correctAnswer = function(){
+//       if(this.prompt == 1 ) {
+//         console.log('Yes, it is correct')
+//       } else {
+//        console.log('No, it is not')
+//       } 
+//     }
+//   }
+  
+//   var kim = new Question('Is my name Kim?','1.yes','2.no');
+//   kim.correctAnswer();
+  
+//   var jay = new Question('Is this Jay\'s pen?','1.yes','2.no');
+//   jay.correctAnswer();
+  
+//   var tim = new Question('Is Tim a student?','1.yes','2.no');
+//   tim.correctAnswer();
+
+
+
+// //Expert level
+// 8. After you display the result, display the next random question, so that the game never ends
+// (Hints: write a function for this and call it right after displayint the result)
+
+// 9. Be careful: after Task 8, the game literally never ends. 
+// So include the option to quit the game if the user writes 'exit' instead of the answer.
+// In this case, DON'T call the function from task 8.
+
+// 10. Track the user's score to make the game more fun! So each time an anser is correct, add 1 point to the score
+// (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+// 11. Display the score in the console. Use yet another method for this.
+
+
+
